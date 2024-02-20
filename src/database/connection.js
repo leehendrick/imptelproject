50b42869f9ab6b966af851068f6b02512b15d738
leehendrick <imhendrick16@gmail.com>
2024-02-20 07:56:56 +0100

    Atualizando a conexão da bd

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,32 +1,28 @@
 const mysql = require('mysql2');
-const {release} = require("mysql/lib/PoolConnection");
+const { release } = require("mysql/lib/PoolConnection");
 require('dotenv').config();
 
-function getConnection(){
-    const dbConfig = {
-        host: process.env.DB_HOST || 'localhost',
-        user: process.env.DB_USER || 'root',
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME || 'imptelproject',
-        connectionLimit: 10, // Número máximo de conexões pool
-    };
+const dbConfig = {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME || 'imptelproject',
+    connectionLimit: 10, // Número máximo de conexões pool
+};
 
-    const pool = mysql.createPool(dbConfig);
+const pool = mysql.createPool(dbConfig);
 
-//Usando o pool para obter conexão
-    pool.getConnection((error,connection) => {
-        if (error){
-            console.error("[###ERRO AO CONECTAR A BASE DE DADOS###]:", error);
-        }else{
-            console.log("[...MYSQL CONECTADO...]");
+//Obtendo uma conexão do pool
+pool.getConnection((error, connection) => {
+    if (error) {
+        console.error("[###ERRO AO CONECTAR A BASE DE DADOS###]:", error);
+    } else {
+        console.log("[...MYSQL CONECTADO...]");
 
-            //Liberando a conexão do pool
-            connection.release();
-        }
+        //Liberando a conexão do pool
+        connection.release();
+    }
+});
 
-    });
-
-}
-
-
-module.exports = getConnection;
+// Exportando a instância do pool
+module.exports = pool;
