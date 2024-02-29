428b5d3a4c2698828f395eb8081e235fa2506dba
leehendrick <imhendrick16@gmail.com>
2024-02-29 13:45:16 +0100

    adicionando o middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies['token'];
+
+    console.log('VERIFICANDO O TOKEN: ',token)
+    if (!token) {
+        console.log('TOKEN NÂO FORNECIDO');
+        return res.render('login', {
+            message: '',
+            title: ''
+        });
+    }
+
+    const secretKey = process.env.JWT_SECRET;
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            console.error('Erro ao verificar token:', err);
+            return res.status(401).json({ message: 'Token inválido' });
+        }
+
+        console.log('Token verificado com sucesso. Dados decodificados:', decoded);
+        req.userId = decoded.userId;
+        next();
+    });
+};
+
+module.exports = verifyToken;
