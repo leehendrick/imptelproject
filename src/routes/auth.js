e437c3192eaffea7ebb39c31788dd21a2f8b057b
leehendrick <imhendrick16@gmail.com>
2024-03-01 13:10:19 +0100

    adicionando endpoint do logout

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/auth')
 //Quando o formulário register for acionado executar a função register no controller auth.
 router.post('/register', authController.register);
 router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 module.exports = router;
